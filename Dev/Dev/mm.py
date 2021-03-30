3a9b7116961429e37ed08233e715b162a6cfab38
Rajiv Mehta <rajiv.mehta@live.in>
2021-03-30 14:41:44 +0530

    V 2.0

---
+++ Dev/Dev/mm.py
@@ -0,0 +1,67 @@
+o = ('hello ')
+
+z  = ('world ')
+
+print(o + z)
+
+q = ('hi ')
+
+a = ('mantra')
+
+print(q + a)
+ 
+y = ('sup ')
+
+x = ('mantra')
+
+print(y + x)  
+                 
+t = ('for ')
+
+i = ('theme ')
+
+j = ('ctrl +  ')
+
+g = ('k')
+
+s = (' + t')
+
+print(t + i + j + g + s)
+
+b = ('it')  
+
+h = (' is')
+
+v = (' working')
+
+print(b + h + v)
+
+p = ("i ")
+
+c = ("want ")
+
+w = ("learn ")
+
+v = ("streamlit")
+
+print(p + c + w + v)
+
+e = ("welcome ")
+
+r = ("to ")
+
+u = (" laptop")
+
+d = (" :D")
+
+print(e + r + u + d)    
+
+f = ("it ")
+
+k = ("is ")
+
+l = ("debugging")
+
+n = (" ;)") 
+
+print(f + k + l + n)  
